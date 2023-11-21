5ef1c3e3fe68f7384b1f87f9cddd9e2c8ecfc3e4
r4cken <liljaellie@gmail.com>
2023-11-21 02:11:11 +0100

    Updated translation.

--- Locale/enUS.lua
+++ Locale/enUS.lua
@@ -14,7 +14,7 @@ if L then
     L["lockouts"] = "Lockouts"
     L["noLockouts"] = "No lockouts"
     L["noTrackingAvailable"] = "No tracking information available"
-    L["noTrackingAvailableDescription1"] = "RackensTracker has not seen any max level characters log in to the game so it has no instance lockout or currency information available for display."
+    L["noTrackingAvailableDescription1"] = "RackensTracker has not seen any max level characters log in to the game so it has no quest, instance lockout or currency information available for display."
     L["noTrackingAvailableDescription2"] = "You must log in to a max level character (level 80) to display tracking information. Only max level characters are currently tracked. This will change in the future through AddOn options."
     L["raids"] = "Raids"
     L["dungeons"] = "Dungeons"
