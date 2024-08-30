747c79467f84aecad646161c8cba7f0606e3667f
r4cken <liljaellie@gmail.com>
2024-08-30 05:03:28 +0200

    Updated CharacterUtil
    Removed unused function
    Removed alias annotation in favor of ClassFile type annotation

--- core/util/characterutil.lua
+++ core/util/characterutil.lua
@@ -2,47 +2,26 @@
 local RT = select(2, ...)
 
 local UnitClassBase = UnitClassBase
-local CreateAtlasMarkup, CreateAtlasMarkupWithAtlasSize, GetClassAtlas =
-	  CreateAtlasMarkup, CreateAtlasMarkupWithAtlasSize, GetClassAtlas
+local CreateAtlasMarkup, GetClassAtlas =
+	  CreateAtlasMarkup, GetClassAtlas
 
 ---@class CharacterUtil
 local CharacterUtil = {}
 
----@alias ClassBaseName
----| '"DEATHKNIGHT"'
----| '"DRUID"'
----| '"HUNTER"'
----| '"MAGE"'
----| '"PALADIN"'
----| '"PRIEST"'
----| '"ROGUE"'
----| '"SHAMAN"'
----| '"WARLOCK"'
----| '"WARRIOR"'
-
 --- Returns The player's locale-independent class name
----@return ClassBaseName class
+---@return ClassFile class
 function CharacterUtil:GetCharacterClass()
     local classFilename, _ = UnitClassBase("player")
     return classFilename
 end
 
----@param class ClassBaseName
+---@param class ClassFile
 ---@param iconSize number icon size in UI pixels
 ---@return string icon
 --- Retrieves a class icon with given size for the localized class name provided.
 function CharacterUtil:GetCharacterIcon(class, iconSize)
 	local textureAtlas = GetClassAtlas(class)
 	local icon = CreateAtlasMarkup(textureAtlas, iconSize, iconSize)
-	return icon
-end
-
----@param iconSize number icon size in UI pixels
----@return string icon
---- Retrieves the texture markup string for the texture 'UI-EquipmentManager-Toggle'
-function CharacterUtil:GetEquipmentIcon(iconSize)
-	iconSize = iconSize or 22
-	local icon = CreateSimpleTextureMarkup([[Interface\PaperDollInfoFrame\UI-EquipmentManager-Toggle]], iconSize, iconSize)
 	return icon
 end
 
